375d1f24ceb4c8391fbeee5d24370c59c4ae482b
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-04-26 02:09:22 +0530

    authentication routes added

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -4,4 +4,8 @@ bp = Blueprint("user", __name__)
 
 @bp.route("/login")
 def login():
-    return render_template("index.html")
+    return render_template("login.html")
+
+@bp.route("/register")
+def register():
+    return render_template("register.html")
