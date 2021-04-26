db86c427afa31d9774fe28129ece016cbd2e9a02
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-04-26 19:28:57 +0530

    index completed

--- templates/index.js
+++ templates/index.js
@@ -1,28 +1,25 @@
-function dateHelper() {
-  
-  var element = document.getElementById("reportrange");
-  var start = moment().subtract(29, 'days');
-  var end = moment();
+$(function() {
 
-  function cb(start, end) {
-      $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-  }
+    var start = moment().subtract(29, 'days');
+    var end = moment();
 
-  element.daterangepicker({
-      startDate: start,
-      endDate: end,
-      ranges: {
-          'Today': [moment(), moment()],
-          'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-          'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-          'This Month': [moment().startOf('month'), moment().endOf('month')],
-          'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-      }
-  }, cb);
+    function cb(start, end) {
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    }
 
-  cb(start, end);
-  
-};
+    $('#reportrange').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+           'Today': [moment(), moment()],
+           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'This Month': [moment().startOf('month'), moment().endOf('month')],
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
 
-dateHelper()
+    cb(start, end);
+    
+});
