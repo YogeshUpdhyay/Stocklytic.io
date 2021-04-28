4f5759ff4f87b946a0f34701a0a3fd050aa4329f
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-04-28 11:57:26 +0530

    auth model defined

---
+++ src/models/auth.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String
+from .src import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    fullname = Column(String)
+    email = Column(String)
+    password = Column(String)
+
+    def __repr__(self):
+        return "<User(fullname='%s', email='%s', password='%s')>" % (
+                             self.fullname, self.email, self.password)
+                            
+    def setpassword(self, password):
+        pass
+
+    def verifypassword(self, password):
+        pass
