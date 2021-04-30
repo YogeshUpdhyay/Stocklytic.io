1d3b3c2e515b601097b0d6d638f54e9906251af8
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-04-30 22:52:18 +0530

    individual route updated, css updated

--- src/routes/stock.py
+++ src/routes/stock.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 bp = Blueprint("stock", __name__)
 
-@bp.route("/stock/{id}")
-def login():
+@bp.route("/stock/<id>")
+def login(id):
     return render_template("stock.html")
