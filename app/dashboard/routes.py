69b7693e6d5e6fec950e47f051dda70e05f6b0cd
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-05-03 17:58:40 +0530

    protected view added

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -1,13 +1,14 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from jinja2 import TemplateNotFound
-from flask_login import current_user
+from flask_login import current_user, AnonymousUserMixin, login_required
 
 bp = Blueprint("dashboard", __name__)
 
-
 @bp.route("/")
+@login_required
 def index():
     return render_template("index.html", current_user=current_user)
+
 
 @bp.route('/<template>')
 def route_template(template):
