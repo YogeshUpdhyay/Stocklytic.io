cde1d26a394b9bb9cc6b37d14c47f48da52f1800
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-05-11 22:05:42 +0530

    forgot password view added

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import TextField, PasswordField
+from wtforms import validators
 from wtforms.validators import InputRequired, Email, DataRequired
 
 ## login and registration
@@ -12,3 +13,6 @@ class CreateAccountForm(FlaskForm):
     username = TextField('Username' , id='username_create' , validators=[DataRequired()])
     email    = TextField('Email' , id='email_create' , validators=[DataRequired(), Email()])
     password = PasswordField('Password' , id='pwd_create' , validators=[DataRequired()])
+
+class ForgotPasswordForm(FlaskForm):
+    email = TextField('Email', id='email-id', validators=[DataRequired()])
