d392313a9e792ce4c128e26568a4ad3a6926ca75
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-05-11 11:24:17 +0530

    stock form api call added

--- app/dashboard/api.py
+++ app/dashboard/api.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 
 bp = Blueprint("api", __name__, url_prefix="/api/v1")
 
-@bp.route("/stockdetail")
+@bp.route("/stockdetail", methods=["GET"])
 def stock_detail():
-    pass
+    return jsonify(msg="Success")
 
