37f0cb6e9bf5511c3c9284e05ee0665fc98fa316
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-05-18 16:07:20 +0530

    indicaotr frontend updated

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -1,3 +1,5 @@
+import os
+import json
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
 
@@ -9,4 +11,12 @@ bp = Blueprint("dashboard", __name__)
 @login_required
 def index():
     # index page 
-    return render_template("index.html", current_user=current_user)
+
+    # reading indicators
+    indicators = json.load(open(os.path.join(os.getcwd(), 'app/utils/static/indicators.json')))
+
+    return render_template(
+        "index.html", 
+        current_user=current_user, 
+        indicators=indicators
+    )
